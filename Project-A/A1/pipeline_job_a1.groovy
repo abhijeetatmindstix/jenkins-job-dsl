47d5497e1a5847ec376042f0b4a6bb3bd118b768
abhijeetatmindstix <113978688+abhijeetatmindstix@users.noreply.github.com>
2022-11-14 19:54:34 +0530

    Create pipeline_job_a1.groovy

---
+++ Project-A/A1/pipeline_job_a1.groovy
@@ -0,0 +1,31 @@
+pipelineJob('Project-A/A1/pipeline_job_a1') {
+  definition {
+    cpsScmFlowDefinition {
+      scm {
+        gitSCM {
+          userRemoteConfigs {
+            userRemoteConfig {
+              credentialsId('')
+              name('')
+              refspec('')
+              url('https://github.com/SumeetBandgar/Job_DSL.git')
+            }
+          }
+          branches {
+            branchSpec {
+              name('*/pipeline-a1')
+            }
+          }
+          browser {
+            gitWeb {
+              repoUrl('')
+            }
+          }
+          gitTool('')
+        }
+      }
+      scriptPath('Jenkinsfile')
+      lightweight(true)
+    }
+  }
+}
