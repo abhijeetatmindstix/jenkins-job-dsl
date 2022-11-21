bacdc11ff8b3a808f65b4b09334231cf7c27d938
abhijeetatmindstix <113978688+abhijeetatmindstix@users.noreply.github.com>
2022-11-21 12:36:56 +0530

    Update folders.groovy

--- folders.groovy
+++ folders.groovy
@@ -1,24 +1,27 @@
-folder('Project-A') {
+folder('JobDSL') {
+    description('Folder for project A, project B, project c')
+}
+folder('JobDSL/Project-A') {
     description('Folder for project A')
 }
-folder('Project-A/A1') {
+folder('JobDSL/Project-A/A1') {
     description('Project-A => Folder for project A1')
 }
-folder('Project-A/A2') {
+folder('JobDSL/Project-A/A2') {
     description('Project-A => Folder for project A2')
 }
-folder('Project-B') {
+folder('JobDSL/Project-B') {
     description('Folder for project B')
 }
-folder('Project-C') {
+folder('JobDSL/Project-C') {
     description('Folder for project A')
 }
-folder('Project-C/C1') {
+folder('JobDSL/Project-C/C1') {
     description('Project-C => Folder for project C1')
 }
-folder('Project-C/C2') {
+folder('JobDSL/Project-C/C2') {
     description('Project-C => Folder for project C2')
 }
-folder('Project-C/C1/C-1.1') {
+folder('JobDSL/Project-C/C1/C-1.1') {
     description('Project-C => Folder C => Folder for project C 1.1')
 }
